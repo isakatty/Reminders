4306ce3400d8f5ecc546765e91f8bc206d0d6242
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-07-05 20:02:04 +0900

    [Feat] Repository pattern을 적용한 Realm Data CRUD 구현
    - Realm에 저장할 데이터 모델 변경
    - Repository pattern을 적용한 CRUD

--- Reminders/Global/Model/Reminder.swift
+++ Reminders/Global/Model/Reminder.swift
@@ -14,18 +14,29 @@ final class Reminder: Object {
     @Persisted(primaryKey: true) var id: ObjectId
     @Persisted var title: String
     @Persisted var content: String?
-    @Persisted var date: Date?
+    @Persisted var date: Date
+    @Persisted var tag: String?
+    @Persisted var priority: String
+    @Persisted var imageStr: String?
     @Persisted var idDone: Bool
     
     convenience init(
+        id: ObjectId,
         title: String,
-        content: String?,
-        date: Date?
+        content: String? = nil,
+        date: Date,
+        tag: String? = nil,
+        priority: String,
+        imageStr: String? = nil
     ) {
         self.init()
+        self.id = id
         self.title = title
         self.content = content
         self.date = date
+        self.tag = tag
+        self.priority = priority
+        self.imageStr = imageStr
         self.idDone = false
     }
 }
---
+++ Reminders/Global/Repository/ReminderRepository.swift
@@ -0,0 +1,82 @@
+//
+//  ReminderRepository.swift
+//  Reminders
+//
+//  Created by Jisoo Ham on 7/5/24.
+//
+
+import Foundation
+
+import RealmSwift
+
+enum RealmError: Error {
+    case invalidCreate
+    case invalidFetch
+    case invalidDeleteReminderItem
+    case invalidDeleteReminders
+    case invalidUpdate
+    
+}
+
+protocol ReminderRepositoryProtocol {
+    // C
+    func createReminder(_ reminder: Reminder) throws
+    // R
+    func fetchReminders() -> [Reminder]
+    // U
+    func updateReminder(_ reminder: Reminder) throws
+    // D
+    func deleteReminder(_ reminder: Reminder) throws
+    func deleteAllReminder() throws
+}
+
+final class ReminderRepository: ReminderRepositoryProtocol {
+    
+    private let realm = try! Realm()
+    
+    func createReminder(_ reminder: Reminder) throws {
+        do {
+            try realm.write {
+                realm.add(reminder)
+            }
+        } catch {
+            throw RealmError.invalidCreate
+        }
+    }
+    
+    func fetchReminders() -> [Reminder] {
+        return Array(realm.objects(Reminder.self))
+    }
+    
+    func updateReminder(_ reminder: Reminder) throws {
+        do {
+            try realm.write {
+                reminder.idDone.toggle()
+            }
+        } catch {
+            throw RealmError.invalidUpdate
+        }
+    }
+    
+    func deleteReminder(_ reminder: Reminder) throws {
+        do {
+            try realm.write {
+                realm.delete(reminder)
+            }
+        } catch {
+            print("Delete 실패")
+            throw RealmError.invalidDeleteReminderItem
+        }
+    }
+    
+    func deleteAllReminder() throws {
+        do {
+            try realm.write {
+                realm.deleteAll()
+            }
+        } catch {
+            print("realm DB delete Error")
+            throw RealmError.invalidDeleteReminders
+        }
+    }
+}
